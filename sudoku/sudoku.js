6b3dae41fdfa29dfb5ec819c35e0264c6be6f722
AserSJuma <gmugtz@inf.elte.hu>
2022-08-23 16:15:34 +0200

    Some responsive css, html and logic added/changed

--- sudoku/sudoku.js
+++ sudoku/sudoku.js
@@ -1,15 +1,17 @@
 const e = null
 
-const alpha = [
-    [e,e,e,e,e,e,e,e,e],
-    [e,e,e,e,e,e,e,e,e],
-    [e,e,e,e,e,e,e,e,e],
-    [e,e,e,e,e,e,e,e,e],
-    [e,e,e,e,e,e,e,e,e],
-    [e,e,e,e,e,e,e,e,e],
-    [e,e,e,e,e,e,e,e,e],
-    [e,e,e,e,e,e,e,e,e],
-    [e,e,e,e,e,e,e,e,e]
+const emptyBoard = [
+    [e,e,e  ,e,e,e, e,e,e],
+    [e,e,e  ,e,e,e, e,e,e],
+    [e,e,e  ,e,e,e, e,e,e],
+
+    [e,e,e  ,e,e,e, e,e,e],
+    [e,e,e  ,e,e,e, e,e,e],
+    [e,e,e  ,e,e,e, e,e,e],
+
+    [e,e,e  ,e,e,e, e,e,e],
+    [e,e,e  ,e,e,e, e,e,e],
+    [e,e,e  ,e,e,e, e,e,e]
 ]
 
 //True case
@@ -42,6 +44,75 @@ const bd_f = [
     [e,e,e  ,e,e,e, e,e,9]
 ]
 
+var numberSelected = null
+var tileSelected = null
+var errors = 0
+
+window.onload = function () {
+    setBoard()
+}
+
+function setBoard() {
+    for (let i = 1; i <= 9; i++) {
+        let number = document.createElement("div")
+        number.id = i
+        number.innerText = i
+        number.addEventListener("click", selectNumber)
+        number.classList.add("number")
+        document.getElementById("digits").appendChild(number)
+    }
+
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            let tile = document.createElement("div")
+            tile.id = i.toString() + "," + j.toString()
+            if(emptyBoard[i][j] != null) {
+                tile.innerText = parseInt(emptyBoard[i][j])
+                tile.classList.add("tile-start")
+            }
+            if( i == 2 || i == 5) {
+                tile.classList.add("horiz-line")
+            }
+            if( j == 2 || j == 5) {
+                tile.classList.add("vert-line")
+            }
+            tile.addEventListener("click", selectTile)
+            tile.classList.add("tile")
+            document.getElementById("boardArea").append(tile)
+        }
+    }
+}
+
+function selectNumber() {
+    if(numberSelected != null) {
+        numberSelected.classList.remove("number-selected")
+    }
+    numberSelected = this
+    numberSelected.classList.add("number-selected")
+}
+
+function selectTile() {
+    if(numberSelected) {
+        if(this.innerText != "") {
+            return
+        }
+        this.innerText = numberSelected.id
+        let coords = this.id.split(",")
+        let x = parseInt(coords[0]);
+        let y = parseInt(coords[1]);
+        console.log(x + "," + y)
+    }
+
+}
+
+function Initiate() {
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            // Connect HTML board with logic
+        }
+    }
+}
+
 function solve(board) {
     if (solved(board)) {
         return board
@@ -58,7 +129,7 @@ function findSolution(boards){
         return false
     }
     else {
-        const first = boards.shift();
+        const first = boards.shift()
         const tryPath = solve(first)
         if (tryPath != false){
             return tryPath
@@ -87,8 +158,8 @@ function nextBoards(board){
         const y = firstEmpty[0]
         const x = firstEmpty[1]
         for (let i = 1; i <= 9; i++){
-            const newBoard = [...board];
-            const row = [...newBoard[y]];
+            const newBoard = [...board]
+            const row = [...newBoard[y]]
             row[x] = i
             newBoard[y] = row
             foo.push(newBoard)
@@ -101,7 +172,7 @@ function findEmptySqr(board){
     for (let i = 0; i < 9; i++){
         for (let j = 0; j < 9; j++){
             if (board[i][j] == null) {
-                return [i, j]
+                return [i, j];
             }
         }
     }
@@ -111,7 +182,7 @@ function keepValid(boards){
     const res = [];
     for (let i = 0; i < boards.length; i++){
         if (validBoard(boards[i])){
-            res.push(boards[i])
+            res.push(boards[i]);
         }
     }
     return res
@@ -124,10 +195,10 @@ function goodRow(board){
         const current = [];
         for (let j = 0; j < 9; j++){
             if (current.includes(board[i][j])){
-                return false
+                return false;
             }
             else if (board[i][j] != null){
-                current.push(board[i][j])
+                current.push(board[i][j]);
             }
         }
     }
@@ -139,10 +210,10 @@ function goodCol(board){
         const current = [];
         for (let j = 0; j < 9; j++){
             if (current.includes(board[j][i])){
-                return false
+                return false;
             }
             else if (board[j][i] != null){
-                current.push(board[j][i])
+                current.push(board[j][i]);
             }
         }
     }
@@ -163,10 +234,10 @@ function goodInside(board){
                 coords[0] += y
                 coords[1] += x
                 if (cur.includes(board[coords[0]][coords[1]])){
-                    return false
+                    return false;
                 }
                 else if (board[coords[0]][coords[1]] != null){
-                    cur.push(board[coords[0]][coords[1]])
+                    cur.push(board[coords[0]][coords[1]]);
                 }
             }
         }
@@ -175,7 +246,67 @@ function goodInside(board){
 }
 
 function validBoard(board){
-    return goodRow(board) && goodCol(board) && goodInside(board)
+    return goodRow(board) && goodCol(board) && goodInside(board);
 }
 
-console.log(solve(bd_f))
+//console.log(solve(bd_f))
+
+/*
+document.addEventListener('click', (e) => {
+    console.log(e.target.value)
+})*/
+
+
+function start() {
+    var startingBoard = [[]];
+    var j = 0;
+    for (let i = 1; i <= 81; i++) {
+        const val = document.getElementById(String(i)).value
+        if (val == null) {
+            startingBoard[j].push(null)
+        } else {
+            startingBoard[j].push(Number(val))
+        }
+        if (i % 9 == 0 && i < 81) {
+            startingBoard.push([])
+            j++
+        }
+    }
+    console.log(startingBoard)/*
+    const inputValid = validBoard(startingBoard)
+    if (!inputValid) {
+        inputIsInvalid()
+    } else {
+        const answer = solve(startingBoard)
+        Update(answer, inputValid)
+    }
+*/
+}
+
+function inputIsInvalid(){
+    for (let i = 0; i <= 9; i++) {
+        document.getElementById("row " + String(i)).innerHTML = "The Board is invalid"
+    }
+}
+
+function Update(board) {
+    if (board == false) {
+        for(let i = 1; i <= 9 ; i++) {
+            document.getElementById("row " + String(i)).innerHTML = "No Solution Exists"
+        }
+    }
+    else {
+        for (let j = 0; j <= 9; j++) {
+            let row =""
+            for (let i = 0; i < 9; i++) {
+                if(row == "") {
+                    row = row + String(board[i - 1][j])
+                }
+                else {
+                    row = row + "\xa0\xa0\xa0\xa0\xa0\xa0\xa0" + String(board[i - 1][j])
+                }
+            }
+            document.getElementById("row " + String(j)).innerHTML = row
+        }
+    }
+}
